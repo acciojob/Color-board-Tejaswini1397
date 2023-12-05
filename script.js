4b2b6733fed49c5bc387aa373566aa25ba58c91f
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-12-05 09:44:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const container = document.querySelector('.container');
+
+    // Create 800 boxes
+    for (let i = 0; i < 800; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+
+        // Add event listener to reset color after 1 second
+        square.addEventListener('mouseover', function () {
+            square.style.backgroundColor = '#e74c3c';
+            setTimeout(() => {
+                square.style.backgroundColor = '#3498db';
+            }, 1000);
+        });
+    }
+});
